d11608e71ed8a343864722b07900908c0ff5d68a
Matilde Aarenstrup Christoffersen <mati1398@edu.easj.dk>
2023-03-02 12:11:44 +0100

    Add project files.

---
+++ CarsLib/Car.cs
@@ -0,0 +1,40 @@
+﻿namespace CarsLib
+{
+    public class Car
+    {
+        public  int Id { get; set; }
+        public string? Model { get; set; }
+        public string? LicensePlate { get; set; }
+        public double Price { get; set; }
+
+
+        public void ValidateModel()
+        {
+            if (Model.Length < 4) throw new ArgumentOutOfRangeException();
+        }
+
+        public void ValidateLicensePlate() 
+        {
+            if(LicensePlate.Length <=2 || LicensePlate.Length >=7) throw new ArgumentOutOfRangeException();
+        }
+
+        public void ValidatePrice()
+        {
+            if(Price <= 0) throw new ArgumentOutOfRangeException();
+        }
+
+        public void Validate()
+        {
+            ValidateLicensePlate();
+            ValidateModel();
+            ValidatePrice();
+        }
+
+        public override string ToString()
+        {
+            return Id + " " + Model + " " + LicensePlate + " " + Price;
+        }
+
+
+    }
+}
---
+++ CarsLibTests/CarsTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarsLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsLib.Tests
+{
+    [TestClass()]
+    public class CarsTests
+    {
+
+        private Car carShortModelName = new() { Id = 1, LicensePlate = "WE 567", Model = "he", Price = 30000};
+        private Car carPriceZero = new() { Id = 2, LicensePlate = "DA 123", Model = "Volvo", Price = 0};
+        private Car carShortLicensePlate = new() { Id = 3, LicensePlate = "H", Model = "Volvo", Price = 23444};
+        private Car carLongLicensePlate = new() { Id = 4, LicensePlate = "abcdefghi", Model = "Volvo", Price = 123487};
+        private Car carToStringTest = new() { Id = 5, LicensePlate = "12345", Model = "volvo", Price = 12345};
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carShortModelName.ValidateModel());
+        }
+
+        [TestMethod()]
+        public void ValidateLicensePlateTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carLongLicensePlate.ValidateLicensePlate());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carShortLicensePlate.ValidateLicensePlate());
+
+        }
+
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carPriceZero.ValidatePrice());
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.AreEqual("5 volvo 12345 12345", carToStringTest.ToString());
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carPriceZero.Validate());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carLongLicensePlate.Validate());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carShortLicensePlate.Validate());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carShortModelName.Validate());
+
+        }
+    }
+}
